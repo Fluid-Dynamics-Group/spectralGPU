bd108887ec0506feb59af3a3d913ec80cf369c87
VanillaBrooks <brooks@karlik.org>
2023-05-10 14:38:04 -0700

    cross, curl, fft benchmarks

---
+++ benches/cross.jl
@@ -0,0 +1,39 @@
+include("../src/spectralGPU.jl");
+using .spectralGPU: mesh, fft, markers, initial_condition, state, config, solver, Integrate
+
+using CUDA
+using BenchmarkTools
+
+N = 64
+
+begin
+    local parallel = markers.SingleThreadGPU()
+    local re = 40.
+    local K = mesh.wavenumbers_gpu(N)
+    local cfg = config.create_config(N, re, 0.00001)
+    local st = state.create_state_gpu(N, K, cfg)
+    local U = CuArray(zeros(N, N, N, 3))
+    local U_hat = CuArray(ComplexF64.(zeros(K.kn, N, N, 3)))
+
+    r = @benchmark solver.cross!($parallel, $U, $st.curl; out = $st.dU)
+
+    println("GPU cross")
+    display(r)
+    println("")
+end
+
+begin
+    local parallel = markers.SingleThreadCPU()
+    local re = 40.
+    local K = mesh.wavenumbers(N)
+    local cfg = config.create_config(N, re, 0.00001)
+    local st = state.create_state(N, K, cfg)
+    local U = zeros(N, N, N, 3)
+    local U_hat = ComplexF64.(zeros(K.kn, N, N, 3))
+
+    r = @benchmark solver.cross!($parallel, $U, $st.curl; out = $st.dU)
+
+    println("CPU cross")
+    display(r)
+    println("")
+end
---
+++ benches/fft.jl
@@ -0,0 +1,99 @@
+include("../src/spectralGPU.jl");
+using .spectralGPU: mesh, fft, markers, initial_condition, state, config, solver, Integrate
+
+using CUDA
+using BenchmarkTools
+
+#########
+######### GPU (forward) benchmark
+#########
+
+N = 64
+
+begin
+    parallel = markers.SingleThreadGPU()
+
+    K = mesh.wavenumbers_gpu(N)
+
+    U = CuArray(zeros(N, N, N, 3))
+    U_hat = CuArray(ComplexF64.(zeros(K.kn, N, N, 3)))
+
+    msh = mesh.new_mesh(N)
+    ic = markers.TaylorGreen()
+    initial_condition.setup_initial_condition(parallel, ic, msh, U, U_hat)
+
+    r = @benchmark @views fft.fftn_mpi!($parallel, $U[:, :, :, 1], $U_hat[:, :, :, 1])
+
+    println("gpu forward FFT")
+    display(r)
+    println("")
+end
+
+#########
+######### CPU (forward) benchmark
+#########
+
+begin
+    parallel = markers.SingleThreadCPU()
+
+    K = mesh.wavenumbers(N)
+
+    U = zeros(N, N, N, 3)
+    U_hat = ComplexF64.(zeros(K.kn, N, N, 3))
+
+    msh = mesh.new_mesh(N)
+    ic = markers.TaylorGreen()
+    initial_condition.setup_initial_condition(parallel, ic, msh, U, U_hat)
+
+    r = @benchmark @views fft.fftn_mpi!($parallel, $U[:, :, :, 1], $U_hat[:, :, :, 1])
+
+    println("cpu forward FFT")
+    display(r)
+    println("")
+end
+
+#########
+######### GPU (inverse) benchmark
+#########
+
+begin
+    parallel = markers.SingleThreadGPU()
+
+    K = mesh.wavenumbers_gpu(N)
+
+    U = CuArray(zeros(N, N, N, 3))
+    U_hat = CuArray(ComplexF64.(zeros(K.kn, N, N, 3)))
+
+    msh = mesh.new_mesh(N)
+    ic = markers.TaylorGreen()
+    initial_condition.setup_initial_condition(parallel, ic, msh, U, U_hat)
+
+    r = @benchmark @views fft.ifftn_mpi!($parallel, $K, $U_hat[:, :, :, 1], $U[:, :, :, 1])
+
+    println("gpu inverse FFT")
+    display(r)
+    println("")
+end
+
+#########
+######### CPU (inverse) benchmark
+#########
+
+begin
+    parallel = markers.SingleThreadCPU()
+
+    K = mesh.wavenumbers(N)
+
+    U = zeros(N, N, N, 3)
+    U_hat = ComplexF64.(zeros(K.kn, N, N, 3))
+
+    msh = mesh.new_mesh(N)
+    ic = markers.TaylorGreen()
+    initial_condition.setup_initial_condition(parallel, ic, msh, U, U_hat)
+
+    r = @benchmark @views fft.ifftn_mpi!($parallel, $K, $U_hat[:, :, :, 1], $U[:, :, :, 1])
+
+    println("cpu inverse FFT")
+    display(r)
+    println("")
+end
