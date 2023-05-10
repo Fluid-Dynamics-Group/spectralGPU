85b11fda568b0f00687aac9c8aa1c2d4f07d3e45
VanillaBrooks <brooks@karlik.org>
2023-05-09 21:54:14 -0700

    CUDA cross products, curl, fft, ifft

--- src/initial_condition.jl
+++ src/initial_condition.jl
@@ -4,14 +4,16 @@ using ..mesh: Mesh
 using ..fft: fftn_mpi!
 using ..markers: AbstractInitialCondition, AbstractParallel, TaylorGreen
 
+using CUDA
+
 # catch-all for initial condition setup
 function setup_initial_condition(
     parallel::P, 
     ic::I, 
     mesh::Mesh, 
-    U::Array{Float64, 4}, 
-    U_hat::Array{ComplexF64, 4}
-) where P <: AbstractParallel where I <: AbstractInitialCondition
+    U::XARR,
+    U_hat::FARR,
+) where P <: AbstractParallel where I <: AbstractInitialCondition where XARR <: AbstractArray{Float64, 4} where FARR <: AbstractArray{ComplexF64, 4}
     error("unimplemented initial condition for type " * typeof(ic))
 end
 
@@ -20,12 +22,13 @@ function setup_initial_condition(
     parallel::P, 
     ic::TaylorGreen, 
     mesh::Mesh, 
-    U::Array{Float64, 4}, 
-    U_hat::Array{ComplexF64, 4}
-) where P <: AbstractParallel
-    U[:, :, :, 1] .= sin.(mesh.x) .* cos.(mesh.y) .* cos.(mesh.z)
-    U[:, :, :, 2] .= - cos.(mesh.x) .* sin.(mesh.y) .* cos.(mesh.z)
-    U[:, :, :, 3] .= 0.
+    U::XARR,
+    U_hat::FARR,
+) where P <: AbstractParallel where XARR <: AbstractArray{Float64, 4} where FARR <: AbstractArray{ComplexF64, 4}
+    @CUDA.sync U[:, :, :, 1] .= sin.(mesh.x) .* cos.(mesh.y) .* cos.(mesh.z)
+    @CUDA.sync U[:, :, :, 2] .= cos.(mesh.x) .* sin.(mesh.y) .* cos.(mesh.z)
+    @CUDA.sync U[:, :, :, 2] *= -1
+    @CUDA.sync U[:, :, :, 3] .= 0.
 
     @views for i in 1:3
         fftn_mpi!(parallel, U[:, :, :, i], U_hat[:, :, :, i])
